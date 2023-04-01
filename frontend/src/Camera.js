45f860ed40abf94c79e38d2462d1c2b60228cdd3
akash kollabathula <kollabathulaakash195@gmail.com>
2023-04-01 14:36:20 +0530

    changes in backend

--- frontend/src/Camera.js
+++ frontend/src/Camera.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import img from './assests/images/img0.jpg'
-
+// import img from './img0.jpg'
+ import img from './assests/img0.jpg'
 export default function Camera() {
   return (
     <>
-    <h3 className='text-white font-bold bg-white'>hello world</h3>
-    <div>
-
-        <img src='http://localhost:5000/video' alt='camera' width='750'/>
-        <img src={img} alt='paint' width='750%'/>
+    {/* <h3 className='text-white font-bold bg-white'>hello world</h3> */}
+    <div style={{display:'flex'}}>
+        <img src='http://localhost:5000/video' alt='camera' width='850'/>
+        <img src={img} alt='paint' width='700'/>
     </div>
-    
     </>
   )
 }
