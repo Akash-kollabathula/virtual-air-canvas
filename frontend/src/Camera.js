7d7411d9ae593fd1b1ba202cd7835363b5bde3f3
karthikmalla745 <karthikmalla745@gmail.com>
2023-04-01 16:31:17 +0530

    resolve merge conflicts

--- frontend/src/Camera.js
+++ frontend/src/Camera.js
@@ -4,11 +4,6 @@ import React from 'react'
 export default function Camera() {
   return (
     <>
-    
-    <div>
-
-        <img src='http://localhost:5000/video' alt='camera' width='750'/>
-        <img src={img} alt='paint' width='750%'/>
     {/* <h3 className='text-white font-bold bg-white'>hello world</h3> */}
     <div style={{display:'flex'}}>
         <img src='http://localhost:5000/video' alt='camera' width='850'/>
